dc2f41c2bf0d3134ee452aff24ca664271c2c7e0
Xander Kaylan <xxk3990@rit.edu>
2023-09-12 14:52:58 -0400

    Order items should now handle data correctly

--- server/router.js
+++ server/router.js
@@ -6,12 +6,13 @@ const router = (app) => {
    // app.post('/login', users.login)
  //   app.post('/addUser', users.createAccount)
    // app.get('/verify', mid.verifyRequestAuth, mid.verifySession)
-    app.get('/allProducts', product.getProducts)
-    app.post('/addProduct', product.addProduct)
-    app.get('/orders', order.getOrders)
-    app.post('/addOrder', order.addOrder)
-    app.get('/orderInfo', items.getOrderItems);
-    app.post('/addOrderInfo', items.addItemToOrder)
+    app.get('/allProducts', product.getProducts) //admin only
+    app.post('/addProduct', product.addProduct) //admin only
+    app.get('/userOrders', order.userOrders) //this one is for specific users, need another for admin purposes
+    //app.get('/allOrders', admin.getAllOrders) not created yet
+    app.post('/submitOrder', order.submitOrder)
+    app.get('/orderItems', items.getOrderItems);
+    app.post('/addOrderItem', items.addItemToOrder)
 
 
 }

29c678903d16476584b7f59f383486d82629e903
Xander Kaylan <xxk3990@rit.edu>
2023-09-28 11:14:28 -0400

    image link works

--- client/src/Products.js
+++ client/src/Products.js
@@ -1,16 +1,17 @@
 import React  from 'react';
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect} from 'react';
 import { handleGet, handlePost } from './services/requests-service';
 import './styles/products.css';
+import { Snackbar } from '@mui/material';
 
 export default function Products() {
     const [products, setProducts] = useState([]);
+    const [openSnackbar, setOpenSnackbar] = useState(false);
     const [newProduct, setNewProduct] = useState({
         productName: '',
         imageURL: '',
         price: 0
     })
-    const file = useRef(null);
     const getProducts = async () => {
         const endpoint = `allProducts`
         handleGet(endpoint, setProducts)
@@ -22,12 +23,6 @@ export default function Products() {
 
     const handleChange = (name, value) => {
         setNewProduct({...newProduct, [name]:value})
-    }
-    const handleUpload = (inputName, path, value) => {
-        const imgLink = URL.createObjectURL(path)
-        console.log(imgLink);
-        newProduct.imageURL = imgLink
-        handleChange(inputName, value);
     }
     const postProduct = async() => {
         const endpoint = `addProduct`;
@@ -41,15 +36,16 @@ export default function Products() {
             const data = await response.json()
             if(response.status === 200 || response.status === 201) {
                 setProducts([...products, data])
-                setNewProduct({
-                    productName: '',
-                    imageURL: '',
-                    price: 0
-                })
                 getProducts();
-                if(file.current) {
-                    file.current.value = null;
-                }
+                setOpenSnackbar(true);
+                setTimeout(() => {
+                    setOpenSnackbar(false);
+                    setNewProduct({
+                        productName: '',
+                        imageURL: '',
+                        price: 0
+                    })
+                }, 1500)
 
             } else {
                 alert("An Error occurred during product creation.")
@@ -61,10 +57,11 @@ export default function Products() {
     if(products.length === 0) {
       return (
         <div className='Products'>
+            <Snackbar open={openSnackbar} autoHideDuration={1500} message="Product Added Successfully!" anchorOrigin={{horizontal: "center", vertical:"top"}}/>
             <h4>No products yet!</h4>
             <section className='add-product'>
                 <span className='product-form-question' id="productname">Product Name: <input type="text" className='user-input' name="productName" value={newProduct.productName} onChange={e => handleChange(e.target.name, e.target.value)} required/></span>
-                <span className='product-form-question' id="productimageUrl">Product Image: <input type="file" className='user-input' id="product-img-upload" ref={file} name="imageURL" value={newProduct.imageURL} onChange={e => handleUpload(e.target.name, e.target.files[0], e.target.value)} required/></span>
+                <span className='product-form-question' id="productimageUrl">Product Image: <input type="text" className='user-input' id="product-img" name="imageURL" value={newProduct.imageURL} onChange={e => handleChange(e.target.name, e.target.value)} required placeholder='Please paste a proper link.'/></span>
                 <span className='product-form-question' id="productprice">Product Price: <input type="number" min="0" max="2500" className='user-input' name="price" value={newProduct.price} onChange={e => handleChange(e.target.name, e.target.value)} required/></span>
                 <button type='button' onClick={postProduct}>Submit</button>
             </section>
@@ -73,6 +70,7 @@ export default function Products() {
     } else {
         return (
             <div className='Products'>
+                <Snackbar open={openSnackbar} autoHideDuration={1500} message="Product Added Successfully!" anchorOrigin={{horizontal: "center", vertical:"top"}}/>
                 <h1>All products</h1>
                 <section className='products-grid'>
                     {products.map(p => {
@@ -81,7 +79,7 @@ export default function Products() {
                 </section>
                 <section className='add-product'>
                     <span className='product-form-question' id="productname">Product Name: <input type="text" className='user-input' name="productName" value={newProduct.productName} onChange={e => handleChange(e.target.name, e.target.value)} required/></span>
-                    <span className='product-form-question' id="productimageUrl">Product Image: <input type="file" className='user-input' id="product-img-upload" name="imageURL" ref={file} onChange={e => handleUpload(e.target.name, e.target.files[0], e.target.value)} required/></span>
+                    <span className='product-form-question' id="productimageUrl">Product Image: <input type="text" className='user-input' id="product-img" name="imageURL" onChange={e => handleChange(e.target.name, e.target.value)} required placeholder='Please paste a proper link.'/></span>
                     <span className='product-form-question' id="productprice">Product Price: <input type="number" min="0" max="2500" className='user-input' name="price" value={newProduct.price} onChange={e => handleChange(e.target.name, e.target.value)} required/></span>
                     <button type='button' onClick={postProduct}>Submit</button>
                 </section>
@@ -95,6 +93,7 @@ const Product = (props) => {
     return (
         <section className="product-info">
             <h3 id="productname">{p.product_name}</h3>
+            <img className="product-list-img" src = {p.image_url} alt = {p.product_name}/>
             <p>${p.price}</p>
         </section>
     )

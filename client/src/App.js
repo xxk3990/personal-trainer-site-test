0414e6cabb8849a291de95ac16d15a0e5e25ffad
Xander Kaylan <xxk3990@rit.edu>
2023-10-01 11:23:13 -0400

    navbar implemented!

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,16 @@ import {Routes, Route, Outlet} from 'react-router-dom';
 import ShoppingCart from './ShoppingCart';
 import Orders from './Orders';
 import Products from './Products';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <Routes>
-      <Route path="/shoppingCart" element={<ShoppingCart/>}/>
-      <Route path="/userOrders" element={<Orders/>}/>
-      <Route path="/adminProducts" element={<Products/>}/>
+      <Route element={<> <Navbar/> <Outlet /></>}>
+        <Route path="/shoppingCart" element={<ShoppingCart/>}/>
+        <Route path="/userOrders" element={<Orders/>}/>
+        <Route path="/adminProducts" element={<Products/>}/>
+      </Route>
     </Routes>   
   );
 }

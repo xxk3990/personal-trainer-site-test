3b5dfb23cb3ee8ce08bccb59c8b57374af97ceba
Xander Kaylan <xxk3990@rit.edu>
2023-10-09 20:17:12 -0400

    Trying to get zero to show if cents is < 10. Almost got it

--- server/controllers/controller-utils.js
+++ server/controllers/controller-utils.js
@@ -1,6 +1,6 @@
 const removeDecimalIfNeeded = (amount) => {
     const priceString = amount.toString();
-    if(priceString[priceString.length - 3] === '.') { //if it contains a decimal
+    if(priceString[priceString.length - 3] === '.' || priceString[priceString.length - 2] === '.') { //if it contains a decimal
         const removeDecPoint = priceString.split('.') //separate each side of the decimal point
         const priceWithoutDecimal = `${removeDecPoint[0]}${removeDecPoint[1]}` //combine into single string
         return parseInt(priceWithoutDecimal); //convert combined string to integer
--- server/controllers/orders-controller.js
+++ server/controllers/orders-controller.js
@@ -4,9 +4,22 @@ const {
 const models = require('../models')
 const utils = require('./controller-utils')
 
-const userOrders = async (req, res) => { 
+const userOrders = async (req, res) => {
     const orders = await models.Order.findAll(); //add check for orders based on specific user once users are added
-    if(orders.length !== 0) {
+    if (orders.length !== 0) {
+        orders.forEach(odr => {
+            if (!utils.integerTest(odr.order_total)) {
+                const decimalTotal = Number(odr.order_total).toFixed(2);
+                const totalSplit = decimalTotal.split(".")
+                if (decimalTotal[decimalTotal.length - 2] === ".") {
+                    const zero = 0;
+                    totalSplit[1] = `${totalSplit[1]}${zero}`
+                    odr.order_total = parseFloat(Number(`${totalSplit[0]}.${totalSplit[1]}`).toFixed(2))
+                } else {
+                    odr.order_total = utils.addDecimal(odr.order_total)
+                }
+            }
+        })
         return res.json(orders)
     } else {
         return res.send([]) //send empty response so front-end can check if orders.length === 0
@@ -15,8 +28,7 @@ const userOrders = async (req, res) => {
 
 const submitOrder = async (req, res) => { //add transactional integrity
     const orderItems = req.body.cart_items;
-    const formattedTotal = utils.removeDecimalIfNeeded(req.body.order_total)
-    if(orderItems.length === 0 || !orderItems) { //if there is an issue with the items in their order
+    if (orderItems.length === 0 || !orderItems) { //if there is an issue with the items in their order
         return res.status(400).send()
     } else {
         const orderDate = new Date(req.body.order_date).toISOString();
@@ -24,10 +36,18 @@ const submitOrder = async (req, res) => { //add transactional integrity
             //id, order_date, order_total
             uuid: uuidv4(),
             order_date: orderDate,
-            order_total: formattedTotal,
+            order_total: ""
+        }
+        const totalString = req.body.order_total.toString();
+        if (totalString[totalString - 2] === ".") {
+            const totalSplit = totalString.split(".")
+            totalSplit[1] = `${totalSplit[1]}0`
+            newOrder.order_total = utils.removeDecimalIfNeeded(parseFloat(`${totalSplit[0]}.${totalSplit[1]}`).toFixed(2))
+        } else {
+            newOrder.order_total = utils.removeDecimalIfNeeded(req.body.order_total)
         }
         try {
-            models.sequelize.transaction(async() => {
+            models.sequelize.transaction(async () => {
                 orderItems.map(async item => {
                     await models.Order_Item.create({
                         uuid: uuidv4(),
@@ -35,7 +55,11 @@ const submitOrder = async (req, res) => { //add transactional integrity
                         product_uuid: item.product_uuid,
                         quantity: item.quantity
                     })
-                    await models.Cart_Item.destroy({where: {"product_uuid" : item.product_uuid}})
+                    await models.Cart_Item.destroy({
+                        where: {
+                            "product_uuid": item.product_uuid
+                        }
+                    })
                 })
                 await models.Order.create(newOrder);
                 return res.status(201).send()
@@ -43,8 +67,10 @@ const submitOrder = async (req, res) => { //add transactional integrity
         } catch {
             return res.status(400).send();
         }
-        
     }
 }
 
-module.exports = {userOrders, submitOrder}
+module.exports = {
+    userOrders,
+    submitOrder
+}

92402106fa4d2b1f79850a8b2c53f15425ba6474
Xander Kaylan <xxk3990@rit.edu>
2023-11-24 12:18:17 -0500

    sequelize connection now changes based on NODE_ENV

--- server/app.js
+++ server/app.js
@@ -5,13 +5,12 @@ const cors = require('cors');
 const { expressjwt: jwt } = require("express-jwt");
 const process = require('process')
 const router = require("./router");
-const port = process.env.PORT || 3000
-
+const environment = process.env.NODE_ENV;
 const app = express();
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:3001"}))
+app.use(cors({origin: '*'}))
 app.use(express.json());
-
+const port = process.env.PORT || 3000
 
 const connectToDB = async () => {
     console.log("connectToDB called")
@@ -23,9 +22,25 @@ const connectToDB = async () => {
         process.exit(1);
     }
 }
-
 (async () => {
-    await connectToDB();
+    if(environment === "production") {
+        try {
+            await sequelize.authenticate();
+            console.log("Connection with RDS has been established successfully.");       
+        } catch (error) {
+            console.error("Unable to connect to the RDS database:", error);
+            process.exit(1);
+        }
+    } else {
+        try {
+            await connectToDB();
+            console.log("Local connection to db successful.")
+        } catch {
+            console.log("Unable to connect at all.")
+            process.exit(1);
+        }
+        
+    }
 })();
 router(app);
 app.listen(port, (err) => {

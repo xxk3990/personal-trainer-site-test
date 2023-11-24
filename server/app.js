9e0d4d6e78d80337e4b9e3403218582701318ed9
Xander Kaylan <xxk3990@rit.edu>
2023-11-24 13:39:28 -0500

    removed duplicate console.log()

--- server/app.js
+++ server/app.js
@@ -16,7 +16,6 @@ const connectToDB = async () => {
     console.log("connectToDB called")
     try {
         await sequelize.authenticate();
-        console.log("Connection has been established successfully.");
     } catch (error) {
         console.error("Unable to connect to the database:", error);
         process.exit(1);

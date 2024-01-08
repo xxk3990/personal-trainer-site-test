857e5d3cb97b6c8e698196544e98312b2bca897e
Xander Kaylan <xxk3990@rit.edu>
2024-01-08 11:39:27 -0500

    fixed some wording

--- client/src/components/CreateAccount.js
+++ client/src/components/CreateAccount.js
@@ -1,17 +1,20 @@
 import React, { useState, useMemo, useEffect}  from 'react';
 import { useNavigate } from 'react-router';
 import {useForm} from "react-hook-form"
+import "../styles/create-account.css"
 
 export default function CreateAccount() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+
   useEffect(() => {
     document.title = "Create Account"
   })
+
   const {
     register,
     handleSubmit,
     formState: {errors}
-} = useForm()
+  } = useForm()
   
   const postUser = async (data) => {
     const postURL = `http://localhost:3000/addUser`
@@ -24,7 +27,6 @@ export default function CreateAccount() {
       dob: data.dob,
       user_role: data.role
     }
-    console.log('Params:', requestBody)
     const requestParams = {
       method: 'POST',
       headers: {"Content-Type": 'application/json'},
@@ -33,11 +35,9 @@ export default function CreateAccount() {
     try {
       const response = await fetch(postURL, requestParams)
       if(response.status === 200 || response.status === 201) {
-       // setUsers([...users, data])
         setTimeout(() => {
           navigate('/login'); //redirect to login on successful account creation
         }, 1500)
-        
       } else {
         alert("An error occurred.")
       }
@@ -46,47 +46,49 @@ export default function CreateAccount() {
     }
   
   }
-
   
-    return (
-      <div className="CreateAccount">
-        <section className='add-user'>
-          <h4>Create Account</h4>
-          <form className = "login" onSubmit={handleSubmit(postUser)}>
-            <span className='user-form-question' id="username">
-              Username: <input type='text' name='username' className='user-input' {...register("username", {required: true})}/>
-              {errors.username && <span className='required-note'>This field is required</span>}
-            </span>
-            <span className='user-form-question' id="email">
-              Email: <input type='email' name='email' className='user-input' {...register("email", {required: true})}/>
-              {errors.email && <span className='required-note'>This field is required</span>}
-            </span>
-            <span className='user-form-question' id="password">
-              Password: <input type='password' name='password' className='password-input' {...register("password", {required: true})}/>
-              {errors.password && <span className='required-note'>This field is required</span>}
-            </span>
-            <span className='user-form-question' id="first-name">
-              First name: <input type='name' name='firstname' className='user-input' {...register("firstname", {required: true})}/>
-              {errors.firstname && <span className='required-note'>This field is required</span>}
-            </span>
-            <span className='user-form-question' id="last-name">
-              Last name: <input type='name' name='lastname' className='user-input' {...register("lastname", {required: true})}/>
-              {errors.lastname && <span className='required-note'>This field is required</span>}
-            </span>
-            <span className='user-form-question' id="date-of-birth">Date of Birth: <input type='date' name='dob' className='date-input' min="1970-01-01" max="2024-12-31" {...register("dob", {required: true})}/></span>
-            <span className='user-form-question' id="user-role">
-              Type of User: 
-              <select name='role' className='user-input' {...register("role", {required: true})}>
-                <option>Select User Type</option>
-                <option value="User">User</option>
-                <option value="Admin">Admin</option>
-              </select>
-              {errors.dob && <span className='required-note'>This field is required</span>}
-            </span>
-            <button type='submit'>Submit</button>
-          </form>
-        </section>
-        
-      </div>
-    );
+  return (
+    <div className="CreateAccount">
+      <section className='add-user'>
+        <h4>Create Account</h4>
+        <form className = "login" onSubmit={handleSubmit(postUser)}>
+          <span className='user-form-question' id="username">
+            Username: <input type='text' name='username' className='user-input' {...register("username", {required: true})}/>
+            {errors.username && <span className='required-note'>This field is required</span>}
+          </span>
+          <span className='user-form-question' id="email">
+            Email: <input type='email' name='email' className='user-input' {...register("email", {required: true})}/>
+            {errors.email && <span className='required-note'>This field is required</span>}
+          </span>
+          <span className='user-form-question' id="password">
+            Password: <input type='password' name='password' className='password-input' {...register("password", {required: true})}/>
+            {errors.password && <span className='required-note'>This field is required</span>}
+          </span>
+          <span className='user-form-question' id="first-name">
+            First name: <input type='name' name='firstname' className='user-input' {...register("firstname", {required: true})}/>
+            {errors.firstname && <span className='required-note'>This field is required</span>}
+          </span>
+          <span className='user-form-question' id="last-name">
+            Last name: <input type='name' name='lastname' className='user-input' {...register("lastname", {required: true})}/>
+            {errors.lastname && <span className='required-note'>This field is required</span>}
+          </span>
+          <span className='user-form-question' id="date-of-birth">
+            Date of Birth: <input type='date' name='dob' className='date-input' min="1970-01-01" max="2024-12-31" {...register("dob", {required: true})}/>
+            {errors.dob && <span className='required-note'>This field is required</span>}
+          </span>
+          <span className='user-form-question' id="user-role">
+            Type of User: 
+            <select name='role' className='user-input' {...register("role", {required: true})}>
+              <option>Select User Type</option>
+              <option value="User">User</option>
+              <option value="Admin">Admin</option>
+            </select>
+            {errors.role && <span className='required-note'>This field is required</span>}
+          </span>
+          <button type='submit'>Submit</button>
+        </form>
+      </section>
+      
+    </div>
+  );
 }
